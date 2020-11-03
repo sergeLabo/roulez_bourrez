fa7ee1682a23d72b5f9c04777a1d8eab7af26399
serge <serge@localhost>
2020-11-03 09:35:28 +0100

    pret pour test réel

--- accelerometer_app/main.py
+++ accelerometer_app/main.py
@@ -2,17 +2,26 @@
 # -*- coding: UTF-8 -*-
 
 """
+Bug avec debian 10
+    xclip and xsel - FileNotFoundError: [Errno 2]
+résolu avec
+    sudo apt install xclip
+
 Compilation possible avec java 8 et non avec 11
 sudo update-alternatives --config java
 java -version
-
 export JAVA_HOME=/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64
 
 Le service s'appelle Pong
+
+Ne pas oublier d'autoriser les droits d'écriture dans Paramètres/Applications
+sur Android.
 """
 
 import os
 from time import sleep
+from runpy import run_path
+from threading import Thread
 
 import kivy
 kivy.require('1.11.1')
@@ -27,15 +36,9 @@ from plyer import utils
 from oscpy.client import OSCClient
 from oscpy.server import OSCThreadServer
 
-
-# Dans buildozer.spec
-# services = Pong:service.py
-SERVICE_NAME = u'{packagename}.Service{servicename}'.format(
-    packagename=u'org.kivy.accelbasic',
-    servicename=u'Pong')
-
 print("Platform =", utils.platform)
-ANDROID = utils.platform._platform_android
+ANDROID = utils.platform._platform_android  # retourne True ou False
+print("Android =", ANDROID)
 if not ANDROID:
     from kivy.core.window import Window
     # Simulation de l'écran de mon tél: 1280*720
@@ -46,8 +49,24 @@ if not ANDROID:
 
 from jnius import autoclass
 
+"""
+SERVICE_NAME = u'{packagename}.Service{servicename}'.format(
+    packagename=u'org.kivy.accelerometer',
+    servicename=u'ServicePong')
 
-class AccelerometerTest(BoxLayout):
+Structure = package.domain.package.name.ServiceToto
+avec de buildozer.spec
+package.domain = org.kivy
+package.name = accelerometer
+soit
+org.kivy.accelerometer.ServicePong
+"""
+
+SERVICE_NAME = 'org.kivy.accelerometer.ServicePong'
+print("SERVICE_NAME:", SERVICE_NAME)
+
+
+class Accelerometer(BoxLayout):
 
     activity = NumericProperty(-1)
 
@@ -55,80 +74,85 @@ class AccelerometerTest(BoxLayout):
 
         super().__init__()
         self.app = app
-        freq = 50
+        self.root = self.app.get_running_app()
+        self.freq = self.root.frequency
+        self.save_number = self.root.save_number
         self.sensor_status = 0
-        Clock.schedule_interval(self.update_display, 1/freq)
+        Clock.schedule_interval(self.update_display, 1/self.freq)
+        Clock.schedule_once(self.client_once, 1)
 
-    def on_sensor_status(self):
+    def client_once(self, dt):
+        # Envoi des arguments à service.py
+        self.root.client.send_message(b'/frequency', [self.freq])
+        self.root.client.send_message(b'/save_number', [self.save_number])
+
+    def on_sensor_enable(self):
         """Envoi au service de l'info sensor enable or not"""
 
-        r = self.app.get_running_app()
         if self.sensor_status == 0:
             self.sensor_status = 1
             self.ids.acceleromer_status.text = "Stop Accelerometer"
         elif self.sensor_status == 1:
             self.sensor_status = 0
             self.ids.acceleromer_status.text = "Start Accelerometer"
-        print("Envoi de sensor_status", self.sensor_status)
-        r.client.send_message(b'/sensor_status', [self.sensor_status])
+        print("Envoi de sensor_status:", self.sensor_status)
+        self.root.client.send_message(b'/sensor_enable', [self.sensor_status])
 
     def on_activity(self, act):
-        r = self.app.get_running_app()
-        r.client.send_message(b'/activity', [act])
+        self.root.client.send_message(b'/activity', [act])
 
     def update_display(self, dt):
-        root = self.app.get_running_app()
-        a, b, c, activity, num =    (root.display_list[0],
-                                    root.display_list[1],
-                                    root.display_list[2],
-                                    root.display_list[3],
-                                    root.display_list[4])
+        a, b, c, activity, num = (  self.root.display_list[0],
+                                    self.root.display_list[1],
+                                    self.root.display_list[2],
+                                    self.root.display_list[3],
+                                    self.root.display_list[4])
         self.ids.x_label.text = "X: " + str(a)
         self.ids.y_label.text = "Y: " + str(b)
         self.ids.z_label.text = "Z: " + str(c)
         self.ids.activity_label.text = "Activité: " + str(activity)
         self.ids.number.text = "Indice de boucle: " + str(num)
-        self.ids.file_saved.text = root.file
-        self.ids.activ_sensor.text = f"Capteur actif: {root.sensor}"
+        self.ids.file_saved.text = self.root.file
+        self.ids.activ_sensor.text = f"Capteur actif: {self.root.sensor}"
 
     def do_quit(self):
         self.app.get_running_app().do_quit()
 
 
-class AccelerometerTestApp(App):
+class AccelerometerApp(App):
 
     def build(self):
+        self.frequency = int(self.config.get('accelerometer', 'frequency'))
+        self.save_number = int(self.config.get('accelerometer', 'save_number'))
+
+        return Accelerometer(App)
+
+    def on_start(self):
         self.display_list = [0]*5
         self.file = ""
         self.sensor = ""
         self.service = None
 
         self.server = OSCThreadServer()
-        self.server.listen( address=b'localhost',
-                            port=3003,
-                            default=True)
+        self.server.listen(address=b'localhost',port=3003, default=True)
         self.server.bind(b'/acc', self.on_message)
         self.server.bind(b'/file', self.on_file)
         self.server.bind(b'/sensor', self.on_sensor)
         self.client = OSCClient(b'localhost', 3001)
 
         self.start_service()
-        return AccelerometerTest(App)
 
     def start_service(self):
         if ANDROID:
-            #SERVICE_NAME = f"{'org.kivy.accelbasic'}.Service{'Pong'}"
             self.service = autoclass(SERVICE_NAME)
-            m_activity = autoclass(u'org.kivy.android.PythonActivity').mActivity
+            self.m_activity = autoclass(u'org.kivy.android.PythonActivity').mActivity
             argument = ''
-            self.service.start(m_activity, argument)
+            self.service.start(self.m_activity, argument)
         else:
-            from runpy import run_path
-            from threading import Thread
             # Equivaut à:
             # run_path('./service.py', {'run_name': '__main__'}, daemon=True)
             self.service = Thread(  target=run_path,
-                                    args=('./service.py',),
+                                    args=('service.py',),
                                     kwargs={'run_name': '__main__'},
                                     daemon=True)
             self.service.start()
@@ -138,18 +162,71 @@ class AccelerometerTestApp(App):
         self.sensor = sens.decode('utf-8')
 
     def on_file(self, f):
-        self.file = f.decode('utf-8')
+        # acc_2020_11_01_14_26_46/acc_2020_11_01_14_29_53.npz
+        self.file = f.decode('utf-8')[-51:]
 
     def on_message(self, *args):
         self.display_list = args
 
-    def stop_service(self):
-        if ANDROID:  # android
-            self.service.stop()
-            self.service = None
-        else:  # linux
-            self.client.send_message(b'/stop', [1])
-            sleep(1)
+    def build_config(self, config):
+        config.setdefaults('accelerometer',
+                            {'frequency': 50,
+                             'save_number': 3000})
+
+        config.setdefaults('kivy',
+                            { 'log_level': 'debug',
+                              'log_name': 'accelerometer_%y-%m-%d_%_.txt',
+                              'log_dir': '/sdcard',
+                              'log_enable': '1'})
+
+        config.setdefaults('postproc',
+                            { 'double_tap_time': 250,
+                              'double_tap_distance': 20})
+
+    def build_settings(self, settings):
+        data = """[
+                    {"type": "title", "title":"Configuration de l'accéléromètre"},
+
+                    {"type": "numeric",
+                      "title": "Fréquence",
+                      "desc": "de 1 à 50",
+                      "section": "accelerometer", "key": "frequency"},
+
+                    {"type": "numeric",
+                      "title": "Nombre de valeurs par fichier",
+                      "desc": "de 500 à 9000",
+                      "section": "accelerometer", "key": "save_number"}
+
+                   ]"""
+
+        # self.config est le config de build_config
+        settings.add_json_panel('Accelerometer', self.config, data=data)
+
+    def on_config_change(self, config, section, key, value):
+        if config is self.config:  # du joli python rigoureux
+            token = (section, key)
+
+            # Frequency
+            if token == ('accelerometer', 'frequency'):
+                value = int(value)
+                print("Nouvelle Fréquence:", value)
+                if value < 1: value = 1
+                if value >= 50: value = 50
+                self.frequency = value
+                self.client.send_message(b'/frequency', [value])
+                # Save in ini
+                self.config.set('accelerometer', 'frequency', value)
+
+            # Enregistrement tous les
+            if token == ('accelerometer', 'save_number'):
+                value = int(value)
+                print("Nouveau nombre d'enregistrement par fichier:", value)
+                if value < 500: value = 500
+                if value >= 9000: value = 9000
+                self.save_number = value
+                self.client.send_message(b'/save_number', [value])
+                # Save in ini
+                self.config.set('accelerometer', 'save_number', value)
 
     def on_pause(self):
         print("on_pause")
@@ -159,9 +236,15 @@ class AccelerometerTestApp(App):
         print("on_resume")
 
     def do_quit(self):
-        self.stop_service()
-        AccelerometerTestApp.get_running_app().stop()
+        if ANDROID:
+            self.service.stop(self.m_activity)
+            self.service = None
+        else:
+            self.client.send_message(b'/stop', [1])
+            sleep(1)
+
+        AccelerometerApp.get_running_app().stop()
 
 
 if __name__ == '__main__':
-    AccelerometerTestApp().run()
+    AccelerometerApp().run()
--- accelerometer_app/service.py
+++ accelerometer_app/service.py
@@ -20,8 +20,8 @@ from oscpy.server import OSCThreadServer
 
 print("Platform =", utils.platform)
 ANDROID = utils.platform._platform_android
+print("Android =", ANDROID)
 
-ACTIVITY = 3
 
 class VirtualAccelerometer:
     def __init__(self):
@@ -33,14 +33,15 @@ class VirtualAccelerometer:
         self.z = randint(999, 9999)
 
 
-class Accelerometer:
+class AccelerometerService:
 
     def __init__(self):
-
+        self.tempo = 0.02
+        self.save_number = 500
         self.loop = 1
         self.status = 0
-        self.sensor_enabled = False
-        self.virtual_sensor_enabled = False
+        self.status_msg = ""
+        self.sensor_enabled = 0  # 1=android acc, 2=virtualacc
         self.num = 0
         self.init_acc()
         self.virtual_acceler = None
@@ -48,6 +49,7 @@ class Accelerometer:
         self.stop = 0
         self.init_dir()
         self.init_osc()
+        self.sensor_init()
 
     def init_osc(self):
         """Le serveur peut envoyer
@@ -57,15 +59,30 @@ class Accelerometer:
         self.server.listen('localhost', port=3001, default=True)
         self.server.bind(b'/activity', self.on_activity)
         self.server.bind(b'/stop', self.on_stop)
-        self.server.bind(b'/sensor_status', self.on_status)
+        self.server.bind(b'/sensor_enable', self.on_sensor_enable)
+        self.server.bind(b'/frequency', self.on_frequency)
+        self.server.bind(b'/save_number', self.on_save_number)
         self.client = OSCClient(b'localhost', 3003)
 
+    def on_save_number(self, msg):
+        print("Nouvelle valeur pour save_number =", msg)
+        self.save_number = int(msg)
+        self.init_acc()
+        self.num = 0
+
+    def on_frequency(self, msg):
+        print("Nouvelle valeur pour frequency =", msg)
+        self.tempo = 1/int(msg)
+        print(self.tempo)
+        self.init_acc()
+        self.num = 0
+
     def on_activity(self, msg):
-        print("activity", msg)
+        print("Nouvelle valeur pour activity =", msg)
         self.activity = int(msg)
 
     def on_stop(self, msg):
-        print("stop", msg)
+        print("stop =", msg)
         self.loop = int(msg)
 
     def init_dir(self):
@@ -81,99 +98,93 @@ class Accelerometer:
         create_dir(self.my_path_directory)
 
     def init_acc(self):
-        self.acc_x = np.zeros((500,), dtype=int)
-        self.acc_y = np.zeros((500,), dtype=int)
-        self.acc_z = np.zeros((500,), dtype=int)
-        self.acc_act = np.zeros((500,), dtype=int)
+        self.acc_x = np.zeros((self.save_number,), dtype=int)
+        self.acc_y = np.zeros((self.save_number,), dtype=int)
+        self.acc_z = np.zeros((self.save_number,), dtype=int)
+        self.acc_act = np.zeros((self.save_number,), dtype=int)
 
-    def on_status(self, msg):
-        """Appelé si actionné dans main"""
-
-        self.status = int(msg)
-        # Si status = 1, je tourne; si = 0 je bloque
-        if self.status:
-            if not self.sensor_enabled and not self.virtual_sensor_enabled:
-                if ANDROID:
-                    try:
-                        accelerometer.enable()
-                        self.sensor_enabled = True
-                        self.status = "Stop Accelerometer"
-                        print("Andoid sensor enable")
-                    except:
-                        accelerometer.disable()
-                        self.sensor_enabled = False
-                else:
-                    self.status = "Stop Virtual Accelerometer"
-                    self.virtual_sensor_enabled = True
-                    self.virtual_acceler = VirtualAccelerometer()
-                    print("Virtual sensor enabled")
-            else:
-                # Stop de la capture
-                accelerometer.disable()
-                self.sensor_enabled = False
-                self.virtual_sensor_enabled = False
-                self.ids.status_button.text = "Start Accelerometer"
-        else:
-            self.sensor_enabled = False
-            self.virtual_sensor_enabled = False
+    def sensor_init(self):
+        """accelerometer est une variable globale, non reconnue par le thread"""
+        print("Lancement de l'accéléromètre...")
+        if self.sensor_enabled == 0:
+            if ANDROID:
+                try:
+                    accelerometer.enable()
+                    sleep(1)
+                    self.sensor_enabled = 1
+                    self.status_msg = "Stop Accelerometer"
+                    print("Android sensor enable")
+                except:
+                    accelerometer.disable()
+                    self.sensor_enabled = 0
+                    print("Android sensor doesn't work!")
+                    self.status_msg = "Android sensor doesn't work!"
+            else: # Linux
+                self.status_msg = "Stop Virtual Accelerometer"
+                self.sensor_enabled = 2
+                self.virtual_acceler = VirtualAccelerometer()
+                print("Virtual sensor enabled")
 
         # Display
-        if self.sensor_enabled: sens = "Andoid sensor enable"
-        elif self.virtual_sensor_enabled: sens = "Virtual sensor enabled"
+        if self.sensor_enabled == 1: sens = "Andoid sensor enable"
+        elif self.sensor_enabled == 2: sens = "Virtual sensor enabled"
         else : sens = "No sensor"
         self.client.send_message(b'/sensor', [sens.encode('utf-8')])
 
+    def on_sensor_enable(self, msg):
+        """Appelé si actionné dans main avec start et stop accelerometer"""
+        self.status = int(msg)
+
     def get_acceleration(self):
-        a = b = c = 0
+        if self.status:
+            a, b, c = 0,0,0
+            if self.sensor_enabled == 1:
+                val = accelerometer.acceleration[:3]
+                if not val == (None, None, None):
+                    a = int(val[0]*1000)
+                    b = int(val[1]*1000)
+                    c = int(val[2]*1000)
+            elif self.sensor_enabled == 2:
+                self.virtual_acceler.random_acc()
+                a = self.virtual_acceler.x
+                b = self.virtual_acceler.y
+                c = self.virtual_acceler.z
+            else:
+                sleep(1)
+                print("Aucun accéléromètre activé !")
 
-        if self.sensor_enabled:
-            val = accelerometer.acceleration[:3]
-            if not val == (None, None, None):
-                a = int(val[0]*1000)
-                b = int(val[1]*1000)
-                c = int(val[2]*1000)
-        elif self.virtual_sensor_enabled:
-            self.virtual_acceler.random_acc()
-            a = self.virtual_acceler.x
-            b = self.virtual_acceler.y
-            c = self.virtual_acceler.z
-        else:
-            self.sensor_enabled = self.virtual_sensor_enabled = False
-            sleep(1)
-            print("Aucun accéléromètre activé !")
+            if self.sensor_enabled != 0:
+                # Set dans les arrays
+                self.acc_x[self.num] = a
+                self.acc_y[self.num] = b
+                self.acc_z[self.num] = c
+                self.acc_act[self.num] = self.activity
 
-        if self.sensor_enabled or self.virtual_sensor_enabled:
-            # Set dans les arrays
-            self.acc_x[self.num] = a
-            self.acc_y[self.num] = b
-            self.acc_z[self.num] = c
-            self.acc_act[self.num] = self.activity
+                acc_message = [a, b, c, self.activity, self.num]
+                self.client.send_message(b'/acc', acc_message)
 
-            acc_message = [a, b, c, self.activity, self.num]
-            self.client.send_message(b'/acc', acc_message)
-
-            self.num += 1
-            # Fin d'un fichier
-            if self.num >= 500:
-                if self.activity >= 0:
-                    dt = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-                    outfile = f"{self.my_path_directory}/acc_{dt}.npz"
-                    np.savez_compressed(outfile,  **{"x": self.acc_x,
-                                                    "y": self.acc_y,
-                                                    "z": self.acc_z,
-                                                    "activity": self.acc_act})
-                    print(f"Enregistrement de: {outfile}")
-                    self.client.send_message(b'/file', [outfile.encode('utf-8')])
-                else:
-                    a = "Pas d'activité définie"
-                    self.client.send_message(b'/file', [a.encode('utf-8')])
-                self.num = 0
-                self.init_acc()
+                self.num += 1
+                # Fin d'un fichier
+                if self.num >= self.save_number:
+                    if self.activity >= 0:
+                        dt = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+                        outfile = f"{self.my_path_directory}/acc_{dt}.npz"
+                        np.savez_compressed(outfile,  **{"x": self.acc_x,
+                                                        "y": self.acc_y,
+                                                        "z": self.acc_z,
+                                                        "activity": self.acc_act})
+                        print(f"Enregistrement de: {outfile}")
+                        self.client.send_message(b'/file', [outfile.encode('utf-8')])
+                    else:
+                        a = "Pas d'activité définie"
+                        self.client.send_message(b'/file', [a.encode('utf-8')])
+                    self.num = 0
+                    self.init_acc()
 
     def run(self):
         while self.loop:
             self.get_acceleration()
-            sleep(0.02)
+            sleep(self.tempo - 0.0016)
 
 
 def create_dir(my_path_directory):
@@ -182,5 +193,5 @@ def create_dir(my_path_directory):
 
 if __name__ == '__main__':
 
-    ACCELEROMETER = Accelerometer()
+    ACCELEROMETER = AccelerometerService()
     ACCELEROMETER.run()
